5d865896cdc77fc803c557b2542e7ac4d544113e
lst <lst@bu.edu>
2022-03-01 22:32:59 -0500

    format change

--- mini_project.py
+++ mini_project.py
@@ -10,21 +10,16 @@ def read_file(f):
     first_every_2 = ()
     second_every_2 = ()
     line_count = 0
-
     with open(f,'r') as friends:
         content=friends.readlines()
     for row in content:
         stripped_line = row.replace("\n", "") 
         if line_count%2 == 0:
-            first_every_2 += (stripped_line,)
-            
+            first_every_2 += (stripped_line,)          
         elif line_count%2 == 1:
-            second_every_2 += (stripped_line,) 
-            
+            second_every_2 += (stripped_line,)            
         line_count += 1
     return (first_every_2, second_every_2)
-
-name, phone = read_file(f='friends.txt')
 
 def change_format_phone(phone):
     clean_phone=()
@@ -36,23 +31,22 @@ def change_format_phone(phone):
         clean_phone += (line,)
     return clean_phone
 
-clean_phone = change_format_phone(phone)
+if __name__ == "__main__":
+    name, phone = read_file(f='friends.txt')
+    clean_phone = change_format_phone(phone)
+    start_phone = ()
+    for i in clean_phone:
+        state = i[:3]
+        start_phone += (state,)
 
-start_phone = ()
-for i in clean_phone:
-    state = i[:3]
-    start_phone += (state,)
+    code, area = read_file(f='map_areacodes_states.txt')
 
+    mapping = dict(zip(code, area))
 
-############
-code, area = read_file(f='map_areacodes_states.txt')
+    friendarea=()
+    for i in start_phone:
+        state = mapping[i]
+        friendarea += (state,)
 
-mapping = dict(zip(code, area))
-
-friendarea=()
-for i in start_phone:
-    state = mapping[i]
-    friendarea += (state,)
-
-print('You have {} friends!'.format(len(set(name))))
-print('They live in {}'.format(set(friendarea)))
+    print('You have {} friends!'.format(len(set(name))))
+    print('They live in {}'.format(set(friendarea)))
